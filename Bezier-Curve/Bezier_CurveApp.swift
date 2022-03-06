2ccdfcb11edfdf37edf1b33b9f5df5d6d62ae7be
Adin Ackerman <adinackerman@gmail.com>
2022-03-05 18:34:20 -0800

    Initial Commit

---
+++ Bezier-Curve/Bezier_CurveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bezier_CurveApp.swift
+//  Bezier-Curve
+//
+//  Created by Adin Ackerman on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Bezier_CurveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
